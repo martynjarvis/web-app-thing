c7ee2cc2badd8e4e5456ab403cc8fb7ae4d80de4
Martyn Jarvis <martynjarvis@gmail.com>
2013-08-06 18:31:21 +0000

    removed tutorial blog folder

--- blog/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-import settings
-
-app = Flask('blog')
-app.config.from_object('blog.settings')
-
-import views
--- blog/settings.py
+++
@@ -1,4 +0,0 @@
-DEBUG=True
-SECRET_KEY='dev_key_h8hfne89vm'
-CSRF_ENABLED=True
-CSRF_SESSION_LKEY='dev_key_h8asSNJ9s9=+'

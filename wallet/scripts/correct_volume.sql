df52ee5af4e4ebd7e0ba13da18471de7b324d75f
Martyn Jarvis <martynjarvis@gmail.com>
2014-01-16 22:42:42 +0000

    added sql script to correct volume and list tool html page

---
+++ wallet/scripts/correct_volume.sql
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+UPDATE invtypes SET volume=2500 WHERE groupID=324;
+--  AssaultShip
+UPDATE invtypes SET volume=15000 WHERE groupID=419;
+--  Combat Battlecruiser
+UPDATE invtypes SET volume=15000 WHERE groupID=1201;
+--  Attack Battlecruiser
+UPDATE invtypes SET volume=50000 WHERE groupID=27;
+--  Battleship
+UPDATE invtypes SET volume=50000 WHERE groupID=898;
+--  BlackOps
+UPDATE invtypes SET volume=1000000 WHERE groupID=883;
+--  CapitalIndustrialShip
+UPDATE invtypes SET volume=500 WHERE groupID=29;
+--  Capsule
+UPDATE invtypes SET volume=1000000 WHERE groupID=547;
+--  Carrier
+UPDATE invtypes SET volume=10000 WHERE groupID=906;
+--  CombatReconShip
+UPDATE invtypes SET volume=15000 WHERE groupID=540;
+--  CommandShip
+UPDATE invtypes SET volume=2500 WHERE groupID=830;
+--  CovertOps
+UPDATE invtypes SET volume=10000 WHERE groupID=26;
+--  Cruiser
+UPDATE invtypes SET volume=5000 WHERE groupID=420;
+--  Destroyer
+UPDATE invtypes SET volume=1000000 WHERE groupID=485;
+--  Dreadnought
+UPDATE invtypes SET volume=2500 WHERE groupID=893;
+--  ElectronicAttackShips
+UPDATE invtypes SET volume=50000 WHERE groupID=381;
+--  EliteBattleship
+UPDATE invtypes SET volume=3750 WHERE groupID=543;
+--  Exhumer
+UPDATE invtypes SET volume=10000 WHERE groupID=833;
+--  ForceReconShip
+UPDATE invtypes SET volume=1000000 WHERE groupID=513;
+--  Freighter
+UPDATE invtypes SET volume=2500 WHERE groupID=25;
+--  Frigate
+UPDATE invtypes SET volume=10000 WHERE groupID=358;
+--  HeavyAssaultShip
+UPDATE invtypes SET volume=10000 WHERE groupID=894;
+--  HeavyInterdictors
+UPDATE invtypes SET volume=20000 WHERE groupID=28;
+--  Industrial
+UPDATE invtypes SET volume=500000 WHERE groupID=941;
+--  IndustrialCommandShip
+UPDATE invtypes SET volume=2500 WHERE groupID=831;
+--  Interceptor
+UPDATE invtypes SET volume=5000 WHERE groupID=541;
+--  Interdictor
+UPDATE invtypes SET volume=1000000 WHERE groupID=902;
+--  JumpFreighter
+UPDATE invtypes SET volume=10000 WHERE groupID=832;
+--  Logistics
+UPDATE invtypes SET volume=50000 WHERE groupID=900;
+--  Marauders
+UPDATE invtypes SET volume=3750 WHERE groupID=463;
+--  MiningBarge
+UPDATE invtypes SET volume=500 WHERE groupID=1022;
+--  Zephyr
+UPDATE invtypes SET volume=2500 WHERE groupID=237;
+--  Rookieship
+UPDATE invtypes SET volume=500 WHERE groupID=31;
+--  Shuttle
+
+UPDATE invtypes SET volume=2500 WHERE groupID=834;
+--  StealthBomber
+UPDATE invtypes SET volume=5000 WHERE groupID=963;
+--  StrategicCruiser
+UPDATE invtypes SET volume=1000000 WHERE groupID=659;
+--  SuperCarrier
+UPDATE invtypes SET volume=10000000 WHERE groupID=30;
+--  Titan
+UPDATE invtypes SET volume=20000 WHERE groupID=380;
+--  TransportShip
+
+                           
+

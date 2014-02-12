9875707ce67e6763a819501aa9f3001dd4acf7bf
Martyn Jarvis <martynjarvis@gmail.com>
2014-02-12 21:17:03 +0000

    removed old data hack

--- wallet/data.py
+++ wallet/data.py
@@ -1,8 +1,6 @@
 from wallet import app
 # from models import Item
 
-# from google.appengine.ext import ndb
-
 from flask import request,jsonify
     
 @app.route('/data/typeahead')
@@ -12,28 +10,3 @@ def typeahead_data():
     # return jsonify(result=[(x.typeID,x.typeName) for x in data])
     return
     
-# # hack to load eve item data
-# @app.route('/data/loadevedata')
-# def load_eve_data():
-    # with open('./wallet/static/evedata.csv') as f:
-        # # delete old items
-        
-        # # TODO: ndb.delete_multi(list_of_keys)
-        # [item.key.delete() for item in Item.query()]
-            
-        # # add new items
-        # item_list = []
-        # for line in f:
-            # line = line.split('\t')
-            # try :
-                # marketID = int(line[3])
-            # except ValueError:
-                # marketID = None
-            # item = Item(
-                # typeID = int(line[0]),
-                # typeName= str(line[1]),
-                # volume= float(line[2]),
-                # marketGroupID = marketID )
-            # item_list.append(item)
-        # ndb.put_multi(item_list)
-    # return 'done'

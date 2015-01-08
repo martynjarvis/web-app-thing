16d25b2eb2eedd90d6b2b7d83ed1e12543c2e50e
Martyn Jarvis <martynjarvis@gmail.com>
2015-01-08 17:18:30 +0000

    added api mixin, not tested

--- app/api/models.py
+++ app/api/models.py
@@ -1,5 +1,33 @@
 from app import db
 
+# LINKS:
+#  https://gist.github.com/techniq/5174410
+class BaseMixin(object):
+    @classmethod
+    def get_by(cls, **kw):
+        return cls.query.filter_by(**kw).first()
+
+    @classmethod
+    def get_or_create(cls, **kw):
+        r = cls.get_by(**kw)
+        if not r:
+            r = cls(**kw)
+            db.session.add(r)
+        return r
+
+    def populate_from_object(self, obj):
+        for c in self.__table__.columns:
+            setattr(self, c.name, getattr(obj, c.name, default=None))
+
+    def __repr__(self):
+        values = ', '.join("{0}={1}".format(n, getattr(self, n))
+                           for n in self.__table__.c.keys())
+        return "{0}({1})".format(self.__class__.__name__, values)
+
+
+# could add another mixin that deals with assets and orders, where we have to
+# keep track of old orders
+
 class Api(db.Model):
     __tablename__ = 'Api'
     id = db.Column(db.Integer, primary_key=True)
@@ -13,9 +41,6 @@ class Api(db.Model):
 
     character = db.relationship("Character", backref="api_keys")
     corporation = db.relationship("Corporation", backref="api_keys")
-
-    # TODO perhaps as a MixIn we want a populate_from_api(x) where x is the
-    # eveapi object
 
 class Character(db.Model):
     __tablename__ = 'Character'

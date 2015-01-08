4fa70c89f54ced89d71307f3520a8ad5608f43d8
Martyn Jarvis <martynjarvis@gmail.com>
2015-01-08 14:09:45 +0000

    validate api keys

--- app/api/forms.py
+++ app/api/forms.py
@@ -4,18 +4,24 @@ from wtforms import fields
 from wtforms.validators import Email, InputRequired, ValidationError
 import eveapi
 
-#from .models import Api, Character, Corporation
+from .models import Api
 
 class ApiForm(Form):
     id = fields.IntegerField("API ID", validators=[InputRequired()])
     vcode = fields.StringField("VCode", validators=[InputRequired()])
 
-    def validate_api(form, field):
-        auth = eveapi.EVEAPIConnection().auth(keyID=id, vCode=vcode)
+    def validate_id(form, field):
+        api = Api.query.filter(Api.id == field.data).first()
+        if api is not None:
+            raise ValidationError("That API key already exists")
+
+    def validate_vcode(form, field):
+        auth = eveapi.EVEAPIConnection().auth(keyID=form.id.data,
+                                              vCode=form.vcode.data)
         try:
             APIKeyInfo = auth.account.APIKeyInfo()
         except eveapi.Error, e:
-            raise ValidationError("Invalid API key")
+            raise ValidationError("Invalid API.") # TODO better reasons
 
         # keep api information on form object
         form.auth = APIKeyInfo

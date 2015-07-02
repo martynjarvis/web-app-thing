50479e9ea7951d21c6fcc92e2695b0e8dd2628d9
Martyn Jarvis <martynjarvis@gmail.com>
2015-07-02 22:18:41 +0100

    sso: persist oauth cred in session not db

--- app/sso/models.py
+++ app/sso/models.py
@@ -1,7 +1,6 @@
 import time
-from flask.ext.login import UserMixin
 
-from pycrest.eve import AuthedConnection
+from flask.ext.login import UserMixin
 
 from app import db, eve
 
@@ -10,40 +9,9 @@ class User(UserMixin, db.Model):
     character_owner_hash = db.Column(db.String(32, convert_unicode=True), primary_key=True)
     character_id = db.Column(db.Integer)
     character_name = db.Column(db.String(50))
-    expires = db.Column(db.Integer)  # seconds since epoch for compatibility
-    token = db.Column(db.String(50))
-    refresh_token = db.Column(db.String(50))
 
     def __repr__(self):
         return "<User: {}>".format(self.character_name)
 
     def get_id(self):
         return self.character_owner_hash
-
-    def to_connection(self):
-        res = {'access_token': self.token,
-               'refresh_token': self.refresh_token,
-               'expires_in': 0}
-        endpoint = eve._authed_endpoint
-        oauth_endpoint = eve._oauth_endpoint
-        client_id = eve.client_id
-        api_key = eve.api_key
-        con = AuthedConnection(
-            res,
-            endpoint,
-            oauth_endpoint,
-            client_id,
-            api_key,
-        )
-        if self.expires - int(time.time()) < 20:
-            con.refresh()
-            self.expires = con.expires
-            self.token = con.token
-            # self.refresh_token = con.refresh_token 
-            # Refresh is actually always the same within the same
-            # 'authorisation'
-            db.session.commit()
-        else:
-            con.expires = self.expires
-
-        return con
--- app/sso/views.py
+++ app/sso/views.py
@@ -3,10 +3,34 @@ import time
 from flask import Blueprint, redirect, render_template, request, session, url_for
 from flask.ext.login import login_required, login_user, logout_user, current_user
 
+from pycrest.eve import AuthedConnection
+
 from app import eve, db
 from .models import User
 
 sso = Blueprint('sso', __name__)
+
+def get_connection():
+    access_token, refresh_token, expires = session['eve_sso_data']
+    res = {'access_token': access_token,
+           'refresh_token': refresh_token,
+           'expires_in': 0}
+    con = AuthedConnection(
+        res,
+        endpoint=eve._authed_endpoint,
+        oauth_endpoint=eve._oauth_endpoint,
+        client_id=eve.client_id,
+        api_key=eve.api_key,
+        )
+    if expires - int(time.time()) < 20:
+        con.refresh()
+        expires = con.expires
+        refresh_token = con.refresh_token
+        session['eve_sso_data'] = (access_token, refresh_token, expires)
+    else:
+        con.expires = expires
+
+    return con
 
 @sso.route('/login')
 def sso_login():
@@ -23,24 +47,18 @@ def sso_callback():
     user = User.query.get(verify_data['CharacterOwnerHash'])
 
     if user is None:
-        # first log in
+        # First log in
         user = User()
         user.character_owner_hash = unicode(verify_data['CharacterOwnerHash'])
         user.character_id = verify_data['CharacterID']
         user.character_name = verify_data['CharacterName']
-        user.expires = con.expires
-        user.token = con.token
-        user.refresh_token = con.refresh_token
         db.session.add(user)
+        db.session.commit()
 
-    user.expires = con.expires
-    user.token = con.token
-    user.refresh_token = con.refresh_token
-    db.session.commit()
-
+    session['eve_sso_data'] = (con.token, con.refresh_token, con.expires)
     login_user(user)
 
-    return render_template('crest/main.html', data=con.whoami())
+    return redirect(url_for('sso.sso_main'))
 
 @sso.route('/logout/')
 @login_required
@@ -51,5 +69,6 @@ def logout():
 @sso.route('/sso')
 @login_required
 def sso_main():
-    con = current_user.to_connection()
-    return render_template('crest/main.html', data=con.whoami())
+    con = get_connection()
+    return render_template('sso/main.html', data=con.whoami())
+

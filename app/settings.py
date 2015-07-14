908d3ebbd6ad7659f5ae5a7824abf3f771892d09
Martyn Jarvis <martynjarvis@gmail.com>
2015-07-14 22:24:15 +0100

    change to rabbitmq rather than redis as mq backend

--- app/settings.py
+++ app/settings.py
@@ -1,4 +1,6 @@
 SQLALCHEMY_DATABASE_URI='sqlite:///data/main.db'
 #SQLALCHEMY_DATABASE_URI='mysql+pymysql://test:pleaseignore@localhost/test'
-CELERY_BROKER_URL='redis://localhost:6379',
-CELERY_RESULT_BACKEND='redis://localhost:6379'
+
+CELERY_BROKER_URL='amqp://guest:guest@localhost:5672//'
+#CELERY_BROKER_URL='redis://localhost:6379',
+#CELERY_RESULT_BACKEND='redis://localhost:6379'

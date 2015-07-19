c53cb7b54b697e474aade7bfa5b31b0223bbfd20
Martyn Jarvis <martynjarvis@gmail.com>
2015-07-19 14:29:27 +0100

    pyflakes changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,50 +1,61 @@
+"""
+Main script that defines the app
+"""
+# pylint: disable=relative-import, invalid-name
 import datetime
 import eveapi
 import os
 
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.migrate import Migrate, MigrateCommand
-from flask.ext.script import Manager
-from flask.ext.login import LoginManager
+from flask_sqlalchemy import SQLAlchemy  # flask.ext.sqlalchemy
+from flask_migrate import Migrate, MigrateCommand  # flask.ext.migrate
+from flask_script import Manager  # flask.ext.script
+from flask_login import LoginManager  # flask.ext.login
+from flask_wtf.csrf import CsrfProtect
 
 from celery import Celery
 
 import pycrest
 
-import settings
-
 app = Flask('wallet',
             template_folder='app/templates',
             static_folder='app/static')
 
+# === load configuration
 app.config.from_object('app.settings')
 app.config.from_envvar('FLASK_SETTINGS')
 app.config.from_envvar('EVE_SSO_SETTINGS')
 
+# === initialise app extensions
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-# set up auth
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
-# celery stuff
+# === celery stuff
 celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 TaskBase = celery.Task
+
+
 class ContextTask(TaskBase):
+    """Base class for celery tasks."""
     abstract = True
+
     def __call__(self, *args, **kwargs):
         with app.app_context():
             return TaskBase.__call__(self, *args, **kwargs)
 celery.Task = ContextTask
 
-# eveapi stuff
+CsrfProtect(app)
+
+
+# === eveapi stuff
 def eveapi_cast_func(key, value):
-    # attempts to cast an XML string to the most probable type.
+    """Attempts to cast an XML string to the most probable type."""
     cast_funcs = (
         lambda x: datetime.datetime.strptime(x, "%Y-%m-%d %H:%M:%S"),
         int,
@@ -62,7 +73,7 @@ eveapi.set_cast_func(eveapi_cast_func)
 eveapi.set_user_agent("twitter:@_scruff")
 
 
-# eve crest stuff
+# === eve pycrest stuff
 if not os.path.exists(app.config['CREST_CACHE_DIR']):
     os.mkdir(app.config['CREST_CACHE_DIR'])
 
@@ -73,13 +84,18 @@ eve = pycrest.EVE(
     cache_dir=app.config['CREST_CACHE_DIR'])
 eve()  # initialize
 
+# === eve login and authentication stuff
 from .sso.models import User
 login_manager.login_view = "sso.sso_login"
+
+
 @login_manager.user_loader
 def load_user(user_id):
+    """Load user object from user's primary key."""
     u = User.query.get(user_id)
     return u
 
+# === imports to initialise views
 from .sso.views import sso
 app.register_blueprint(sso)
 
@@ -89,4 +105,4 @@ app.register_blueprint(api)
 from .crest.views import crest
 app.register_blueprint(crest)
 
-import views
+import views  # noqa
